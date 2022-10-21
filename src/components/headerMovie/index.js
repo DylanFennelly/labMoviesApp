b3498a1bc347db52e528020f37e37e6caa6962f7
dylanantfennelly@gmail.com <20093427@mail.wit.ie>
2022-10-21 10:10:08 +0100

    Exercise - Show favourite icon on Movie Details page..

--- src/components/headerMovie/index.js
+++ src/components/headerMovie/index.js
@@ -5,11 +5,19 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
+import Avatar from '@mui/material/Avatar';
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const MovieHeader = (props) => {
   const movie = props.movie;
+  const favouriteMovies = JSON.parse(localStorage.getItem("favourites"));
+  const favMovieIds = favouriteMovies.map ((x) => x.id)   //array of ids since movie object and objects in favourite movies are not the same - https://www.w3schools.com/jsref/jsref_map.asp
 
   return (
+    // favouriteMovies.forEach((x,i) => console.log(x["id"])),
+    // console.log(movie["id"]),
+    // console.log(favouriteMovies.includes(movie["id"])),
+    // favouriteMovies.forEach( (x,i) => console.log( x["id"] === movie["id"])),
     <Paper 
         component="div" 
         sx={{
@@ -24,7 +32,15 @@ const MovieHeader = (props) => {
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
 
+      {favMovieIds.includes(movie["id"]) ? (
+            <Avatar sx={{ backgroundColor: 'red' }}>
+              <FavoriteIcon />
+            </Avatar>
+          ) : null         
+      }
+          
       <Typography variant="h4" component="h3">
+        
         {movie.title}
         <a href={movie.homepage}>
           <HomeIcon color="primary" />
