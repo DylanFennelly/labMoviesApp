945a43699b1f687973ec696e51a8c5aefe321cc3
DylanFennelly <20093427@mail.wit.ie>
2022-11-19 17:00:55 +0000

    templateTVPage created

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -115,3 +115,20 @@ export const getMovie = (args) => {
        throw error
     });
   };
+
+  export const getTVImages = ({ queryKey }) => {
+    const [, idPart] = queryKey;
+    const { id } = idPart;
+    return fetch(
+      `https://api.themoviedb.org/3/tv/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then( (response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+  
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
