50ea829f46270a23c62c7db0c07713bd56dc8f0e
DylanFennelly <20093427@mail.wit.ie>
2022-11-26 14:54:21 +0000

    created templateFantasyMovieListPage, fantasyMoveiList and fantasyMovieCard

---
+++ src/components/fantasyMovieList/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import FantasyMovie from "../fantasyMovieCard";
+import Grid from "@mui/material/Grid";
+
+const FantasyMovieList = ( {movies, action }) => {
+  let movieCards = movies.map((m) => (
+    <Grid key={m.id} item xs={12} sm={6} md={4} lg={3} xl={2}>
+      <FantasyMovie key={m.id} movie={m} action={action} />
+    </Grid>
+    
+  ));
+  return movieCards;
+};
+
+export default FantasyMovieList;
