0a80417743c6866728d565e76bff026f8f78addd
DylanFennelly <20093427@mail.wit.ie>
2022-12-03 19:20:47 +0000

    Fantasy Movie and MovieReviews storybooks created

---
+++ src/stories/fantasyMovieHeader.stories.js
@@ -0,0 +1,24 @@
+import React from "react";
+import FantasyHeader from "../components/fantasyMovieHeader";
+import SampleFantasy from "./sampleFantasy";
+import { MemoryRouter } from "react-router";
+import { action } from "@storybook/addon-actions";
+import FantasyContextProvider from "../contexts/fantasyMoviesContext";
+
+export default {
+  title: "Fantasy Movie Details Page/FatansyMovieHeader",
+  component: FantasyHeader,
+  decorators: [
+    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+    (Story) => <FantasyContextProvider>{Story()}</FantasyContextProvider>,
+  ],
+};
+
+export const Basic = () => <FantasyHeader movie={SampleFantasy} />;
+
+Basic.storyName = "Details Page";
+
+export const Form = () => <FantasyHeader movie={null} />;
+  
+Form.storyName = "Fantasy Movie Form";
+  
---
+++ src/stories/movieReviews.stories.js
@@ -0,0 +1,19 @@
+import React from "react";
+import MovieReviews from "../components/movieReviews";
+import { MemoryRouter } from "react-router";
+import SampleMovie from "./sampleData";
+import { action } from "@storybook/addon-actions";
+import MoviesContextProvider from "../contexts/moviesContext";
+
+export default {
+  title: "Movie Details Page/MovieReviews",
+  component: MovieReviews,
+  decorators: [
+    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+    (Story) => <MoviesContextProvider>{Story()}</MoviesContextProvider>,
+  ],
+};
+
+export const Basic = () => <MovieReviews movie={SampleMovie} />;
+
+Basic.storyName = "Default";
