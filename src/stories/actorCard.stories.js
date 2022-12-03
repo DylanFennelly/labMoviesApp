acf261fae4f595bed637e543c085c664b6bfc5c6
DylanFennelly <20093427@mail.wit.ie>
2022-12-03 13:01:49 +0000

    actorCard storybook created

---
+++ src/stories/actorCard.stories.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ActorCard from "../components/actorCard";
+import SampleActor from "./sampleActorData";
+import { MemoryRouter } from "react-router";
+import ActorContextProvider from "../contexts/actorsContext";
+import { action } from "@storybook/addon-actions";
+import AddToActorFavouritesIcon from "../components/cardIcons/addToActorFavourites";
+
+export default {
+  title: "Actor List/ActorCard",
+  component: ActorCard,
+  decorators: [
+    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+    (Story) => <ActorContextProvider>{Story()}</ActorContextProvider>,
+  ],
+};
+
+export const Basic = () => {
+  return (
+    <ActorCard
+      actor={SampleActor}
+      action={(actor) => <AddToActorFavouritesIcon actor={actor} />}
+      taging={(actor) => null}
+    />
+  );
+};
+Basic.storyName = "Default";
+
+export const Exceptional = () => {
+  const sampleNoProfile = { ...SampleActor, profile_path: undefined };
+  return (
+    <ActorCard
+      actor={sampleNoProfile}
+      action={(actor) => <AddToActorFavouritesIcon actor={actor} />}
+      taging={(actor) => null}
+    />
+  );
+};
+Exceptional.storyName = "exception";
