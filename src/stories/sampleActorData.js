204d12063b64eb97231e98d8f2003b52f3b6f2c5
DylanFennelly <20093427@mail.wit.ie>
2022-12-03 12:54:08 +0000

    Additional of SampleActorData

---
+++ src/stories/sampleActorData.js
@@ -0,0 +1,59 @@
+export default{
+    adult: false,
+    gender: 2,
+    id: 1136406,
+    known_for: [
+        {
+            adult: false,
+            backdrop_path: "/tTlAA0REGPXSZPBfWyTW9ipIv1I.jpg",
+            genre_ids: [28, 12, 878, 18],
+            id: 315635,
+            media_type: "movie",
+            original_language: "en",
+            original_title: "Spider-Man: Homecoming", 
+            overview: "Following the events of Captain America: Civil War, Peter Parker, with the help of his mentor Tony Stark, tries to balance his life as an ordinary high school student in Queens, New York City, with fighting crime as his superhero alter ego Spider-Man as a new threat, the Vulture, emerges.",
+            poster_path: "/c24sv2weTHPsmDa7jEMN0m2P3RT.jpg",
+            release_date: "2017-07-05",
+            title: "Spider-Man: Homecoming",
+            video: false,
+            vote_average: 7.4,
+            vote_count: 19327
+        },
+        {
+            adult: false,
+            backdrop_path: "/tTlAA0REGPXSZPBfWyTW9ipIv1I.jpg",
+            genre_ids: [28, 12, 878, 18],
+            id: 315635,
+            media_type: "movie",
+            original_language: "en",
+            original_title: "Spider-Man: Homecoming", 
+            overview: "Following the events of Captain America: Civil War, Peter Parker, with the help of his mentor Tony Stark, tries to balance his life as an ordinary high school student in Queens, New York City, with fighting crime as his superhero alter ego Spider-Man as a new threat, the Vulture, emerges.",
+            poster_path: "/c24sv2weTHPsmDa7jEMN0m2P3RT.jpg",
+            release_date: "2017-07-05",
+            title: "Spider-Man: Homecoming",
+            video: false,
+            vote_average: 7.4,
+            vote_count: 19327
+        },
+        {
+            adult: false,
+            backdrop_path: "/tTlAA0REGPXSZPBfWyTW9ipIv1I.jpg",
+            genre_ids: [28, 12, 878, 18],
+            id: 315635,
+            media_type: "movie",
+            original_language: "en",
+            original_title: "Spider-Man: Homecoming", 
+            overview: "Following the events of Captain America: Civil War, Peter Parker, with the help of his mentor Tony Stark, tries to balance his life as an ordinary high school student in Queens, New York City, with fighting crime as his superhero alter ego Spider-Man as a new threat, the Vulture, emerges.",
+            poster_path: "/c24sv2weTHPsmDa7jEMN0m2P3RT.jpg",
+            release_date: "2017-07-05",
+            title: "Spider-Man: Homecoming",
+            video: false,
+            vote_average: 7.4,
+            vote_count: 19327
+        }
+    ],
+    known_for_department: "Acting",
+    name : "Tom Holland",
+    popularity: 140.268,
+    profile_path: "/bBRlrpJm9XkNSg0YT5LCaxqoFMX.jpg"
+}
