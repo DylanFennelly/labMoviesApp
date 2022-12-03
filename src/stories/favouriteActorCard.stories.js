cb34a3b7a9838b2b7c2830e170493e146ba967a5
DylanFennelly <20093427@mail.wit.ie>
2022-12-03 13:31:24 +0000

    favouriteActorCard and favouriteActorList created

---
+++ src/stories/favouriteActorCard.stories.js
@@ -0,0 +1,39 @@
+import React from "react";
+import FavouriteActorCard from "../components/favouriteActorCard";
+import SampleActor from "./sampleActorData";
+import { MemoryRouter } from "react-router";
+import ActorContextProvider from "../contexts/actorsContext";
+import { action } from "@storybook/addon-actions";
+import AddToActorFavouritesIcon from "../components/cardIcons/addToActorFavourites";
+
+export default {
+  title: "Actor List/FavouriteActorCard",
+  component: FavouriteActorCard,
+  decorators: [
+    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+    (Story) => <ActorContextProvider>{Story()}</ActorContextProvider>,
+  ],
+};
+
+export const Basic = () => {
+  return (
+    <FavouriteActorCard
+      actor={SampleActor}
+      action={(actor) => <AddToActorFavouritesIcon actor={actor} />}
+      taging={(actor) => null}
+    />
+  );
+};
+Basic.storyName = "Default";
+
+export const Exceptional = () => {
+  const sampleNoProfile = { ...SampleActor, profile_path: undefined };
+  return (
+    <FavouriteActorCard
+      actor={sampleNoProfile}
+      action={(actor) => <AddToActorFavouritesIcon actor={actor} />}
+      taging={(actor) => null}
+    />
+  );
+};
+Exceptional.storyName = "exception";
