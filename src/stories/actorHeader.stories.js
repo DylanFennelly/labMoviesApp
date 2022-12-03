508bafc3f0a1a812763b8729082b17bf3e7a442a
DylanFennelly <20093427@mail.wit.ie>
2022-12-03 13:24:52 +0000

    actorHeader storybook created, fix broken movieHeader storybook

---
+++ src/stories/actorHeader.stories.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ActorHeader from "../components/headerActor";
+import Sampleactor from "./sampleActorData";
+import { MemoryRouter } from "react-router";
+import { action } from "@storybook/addon-actions";
+import ActorContextProvider from "../contexts/actorsContext";
+
+export default {
+  title: "Actor Details Page/ActorHeader",
+  component: ActorHeader,
+  decorators: [
+    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+    (Story) => <ActorContextProvider>{Story()}</ActorContextProvider>
+  ],
+};
+
+export const Basic = () => <ActorHeader actor={Sampleactor} />;
+
+Basic.storyName = "Default";
