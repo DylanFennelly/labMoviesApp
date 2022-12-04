b8b29cdcc33b53be32bdac1d606c2703afe00468
DylanFennelly <20093427@mail.wit.ie>
2022-12-04 03:17:21 +0000

    storybooks for new filter cards

---
+++ src/stories/filterFantasyMoviesCard.stories.js
@@ -0,0 +1,31 @@
+import React from "react";
+import FilterFantasyMoviesCard from "../components/filterFantasyMoviesCard";
+import { MemoryRouter } from "react-router";
+import { action } from "@storybook/addon-actions";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export default {
+  title: "Fantasy Movie List/FilterFantasyMoviesCard",
+  component: FilterFantasyMoviesCard,
+  decorators: [
+    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+    (Story) => (
+      <QueryClientProvider client={queryClient}>{Story()}</QueryClientProvider>
+    ),
+  ],
+};
+
+export const Basic = () => {
+  return <FilterFantasyMoviesCard onUserInput={action("filter input")} />;
+};
+Basic.storyName = "Default";
